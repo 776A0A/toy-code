86cf33b762d1e3009e776873bd019a1f33033814
har3dy <har3dy@icloud.com>
2020-08-27 10:14:16 +0800

    添加willWin函数

--- TicTacToe/index.js
+++ TicTacToe/index.js
@@ -3,12 +3,12 @@ const _ = id => document.getElementById(id)
 const board = _('board')
 const result = _('result')
 const pattern = [
-	[0, 0, 0],
-	[0, 0, 0],
+	[1, 1, 0],
+	[2, 0, 0],
 	[0, 0, 0]
 ]
 let done = false,
-	shape = 1,
+	shape = 2,
 	cellMap = ['', '⭕', '❌']
 
 function render() {
@@ -30,46 +30,67 @@ function move(y, x) {
 	if (pattern[y][x] || done) return
 	pattern[y][x] = shape
 	render()
-	if (checkWinner(shape, y, x)) {
+	if (checkWinner(pattern, y, x)) {
 		done = true
 		result.textContent = `Winner Is: ${cellMap[shape]}`
 		return
 	}
 	shape = 3 - shape
+	const res = willWin()
+	if (res) {
+		console.log(
+			`If you(${cellMap[shape]}) move to ${JSON.stringify(res)}, you will win!`
+		)
+	}
 }
 
-function checkWinner(shape, y, x) {
+function willWin() {
+	for (let y = 0; y < 3; y++) {
+		for (let x = 0; x < 3; x++) {
+			if (pattern[y][x]) continue
+			const _pattern = clone(pattern)
+			_pattern[y][x] = shape
+			if (checkWinner(_pattern, y, x)) return { y, x }
+		}
+	}
+}
+
+function checkWinner(pattern, y, x) {
 	return (
-		horizontal(shape, y, x) ||
-		portrait(shape, y, x) ||
-		crosswise1(shape, y, x) ||
-		crosswise2(shape, y, x)
+		horizontal(pattern, y, x) ||
+		portrait(pattern, y, x) ||
+		crosswise1(pattern, y, x) ||
+		crosswise2(pattern, y, x)
 	)
 }
 
-function horizontal(shape, y, x) {
+function horizontal(pattern, y, x) {
 	for (let i = 0; i < 3; i++) {
 		if (pattern[y][i] !== shape) return false
 	}
 	return true
 }
-function portrait(shape, y, x) {
+function portrait(pattern, y, x) {
 	for (let i = 0; i < 3; i++) {
 		if (pattern[i][x] !== shape) return false
 	}
 	return true
 }
-function crosswise1(shape, y, x) {
+function crosswise1(pattern, y, x) {
 	for (let i = 0; i < 3; i++) {
 		if (pattern[i][i] !== shape) return false
 	}
 	return true
 }
-function crosswise2(shape, y, x) {
+function crosswise2(pattern, y, x) {
 	for (let i = 0; i < 3; i++) {
 		if (pattern[i][2 - i] !== shape) return false
 	}
 	return true
+}
+
+function clone(pattern) {
+	return JSON.parse(JSON.stringify(pattern))
 }
 
 render()
