a56a77289e765173729166f3c8bed8a53453346e
har3dy <har3dy@icloud.com>
2020-08-28 20:15:41 +0800

    添加显示路线功能，并修复穿墙的bug

--- path/index.js
+++ path/index.js
@@ -8,43 +8,46 @@ let cells = localStorage.map
 
 async function findPath(map, start, end) {
 	const queue = [start]
+	map.children[start[1] * 100 + start[0]].style.backgroundColor = 'darkviolet'
 	map.children[end[1] * 100 + end[0]].style.backgroundColor = 'lightcoral'
 	while (queue.length) {
-		const [x, y] = queue.shift()
+		let [x, y] = queue.shift()
 		if (x === end[0] && y === end[1]) {
-			map.children[end[1] * 100 + end[0]].style.backgroundColor = 'lightcoral'
+			map.children[start[1] * 100 + start[0]].style.backgroundColor =
+				'darkviolet'
+			while (!(x === start[0] && y === start[1])) {
+				await sleep(1)
+				map.children[y * 100 + x].style.backgroundColor = 'red'
+				;[x, y] = cells[y * 100 + x]
+			}
 			return true
 		}
 		// 上下左右
-		await insert([x, y + 1])
-		await insert([x, y - 1])
-		await insert([x - 1, y])
-		await insert([x + 1, y])
+		await insert([x, y + 1], [x, y])
+		await insert([x, y - 1], [x, y])
+		await insert([x - 1, y], [x, y])
+		await insert([x + 1, y], [x, y])
 
 		// 斜方向
-		if (!cells[(x, y + 1)]) {
-			await insert([x - 1, y + 1])
-			await insert([x + 1, y + 1])
-		} else if (!cells[(x, y - 1)]) {
-			await insert([x - 1, y - 1])
-			await insert([x + 1, y - 1])
-		} else if (!cells[(x - 1, y)]) {
-			await insert([x - 1, y + 1])
-			await insert([x - 1, y - 1])
-		} else if (!cells[(x + 1, y)]) {
-			await insert([x + 1, y + 1])
-			await insert([x + 1, y - 1])
+		if (!cells[(x, y + 1)] || !cells[(x - 1, y)]) {
+			insert([x - 1, y + 1], [x, y])
+		} else if (!cells[(x, y + 1)] || !cells[(x + 1, y)]) {
+			insert([x + 1, y + 1], [x, y])
+		} else if (!cells[(x - 1, y)] || !cells[(x, y - 1)]) {
+			insert([x - 1, y - 1], [x, y])
+		} else if (!cells[(x + 1, y)] || !cells[(x, y - 1)]) {
+			insert([x + 1, y - 1], [x, y])
 		}
 	}
 
 	return false
 
-	async function insert([x, y]) {
+	async function insert([x, y], pre) {
 		if (cells[y * 100 + x]) return
 		if (x < 0 || x >= 100 || y < 0 || y >= 100) return
 		await sleep(1)
 		map.children[y * 100 + x].style.backgroundColor = 'lightseagreen'
-		cells[y * 100 + x] = 2
+		cells[y * 100 + x] = pre
 		queue.push([x, y])
 	}
 }
