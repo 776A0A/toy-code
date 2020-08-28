50f8f401c11ceddee764190134850607ba5d6dce
har3dy <har3dy@icloud.com>
2020-08-28 10:17:25 +0800

    添加保存和清空功能

--- path/index.js
+++ path/index.js
@@ -1,17 +1,27 @@
 const board = document.getElementById('board')
+const save = document.getElementById('save')
+const reset = document.getElementById('reset')
 let down, clear
+const cells = localStorage.board
+	? JSON.parse(localStorage.board)
+	: Array(10000).fill(0)
 
 function render() {
-	const cells = Array(10000).fill(0)
+	board.innerHTML = ''
 	const frag = document.createDocumentFragment()
 	cells.forEach((item, i) => {
 		const cell = document.createElement('div')
+		if (item) {
+			cell.style.backgroundColor = 'black'
+		}
 		cell.classList.add('cell')
 		cell.addEventListener('mousemove', e => {
 			if (down) {
 				if (clear) {
+					cells[i] = 0
 					cell.style.backgroundColor = ''
 				} else {
+					cells[i] = 1
 					cell.style.backgroundColor = 'black'
 				}
 			}
@@ -20,6 +30,17 @@ function render() {
 	})
 	board.append(frag)
 }
+save.addEventListener('click', e => {
+	localStorage.board = JSON.stringify(cells)
+})
+reset.addEventListener('click', e => {
+	const answer = confirm('确认清空？')
+	if (answer) {
+		cells.forEach((_, i) => (cells[i] = 0))
+		save.click()
+		render()
+	}
+})
 
 document.addEventListener('mousedown', e => {
 	down = true
