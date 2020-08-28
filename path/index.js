e85f91e5bcab2941867da017ae6e1047d6b3c2ef
har3dy <har3dy@icloud.com>
2020-08-28 19:13:59 +0800

    添加基础的广度优先寻路算法

--- path/index.js
+++ path/index.js
@@ -6,8 +6,35 @@ let cells = localStorage.map
 	? JSON.parse(localStorage.map)
 	: Array(10000).fill(0)
 
-function path(map, start, end) {
-  
+async function findPath(map, start, end) {
+	const queue = [start]
+	map.children[end[1] * 100 + end[0]].style.backgroundColor = 'lightcoral'
+	while (queue.length) {
+		const [x, y] = queue.shift()
+		if (x === end[0] && y === end[1]) {
+			map.children[end[1] * 100 + end[0]].style.backgroundColor = 'lightcoral'
+			return true
+		}
+		await insert([x - 1, y])
+		await insert([x + 1, y])
+		await insert([x, y - 1])
+		await insert([x, y + 1])
+	}
+
+	return false
+
+	async function insert([x, y]) {
+		if (cells[y * 100 + x]) return
+		if (x < 0 || x >= 100 || y < 0 || y >= 100) return
+		await sleep(1)
+		map.children[y * 100 + x].style.backgroundColor = 'lightseagreen'
+		cells[y * 100 + x] = 2
+		queue.push([x, y])
+	}
+}
+
+function sleep(t) {
+	return new Promise(resolve => setTimeout(resolve, t))
 }
 
 function render() {
@@ -34,10 +61,7 @@ function render() {
 	})
 	map.append(frag)
 }
-save.addEventListener(
-	'click',
-	e => (localStorage.map = JSON.stringify(cells))
-)
+save.addEventListener('click', e => (localStorage.map = JSON.stringify(cells)))
 reset.addEventListener('click', e => {
 	const answer = confirm('确认清空？')
 	if (answer) {
