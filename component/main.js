737b67f889fe2b7c2e7b61f083406a7549039169
har3dy <har3dy@icloud.com>
2020-09-04 16:29:51 +0800

    使用jsx来渲染carousel

--- component/main.js
+++ component/main.js
@@ -1,20 +1,40 @@
 function create(cls, attributes, ...children) {
-	const c = new cls()
+	let c
+
+	if (typeof cls === 'string') c = new Element(cls)
+	else c = new cls()
+
 	for (const key in attributes) {
 		if (attributes.hasOwnProperty(key)) {
 			c.setAttribute(key, attributes[key])
 		}
 	}
-	for (const child of children) {
-		c.appendChild(child)
+	const append = children => {
+		children.forEach(child => {
+			if (typeof child === 'string') child = new Text(child)
+			else if (Array.isArray(child)) return append(child)
+			c.appendChild(child)
+		})
 	}
+
+	append(children)
+
 	return c
 }
 
-class Component {
-	constructor(config) {
+class Text {
+	constructor(text) {
+		this.root = document.createTextNode(text)
+	}
+	mountTo(parent) {
+		parent.appendChild(this.root)
+	}
+}
+
+class Element {
+	constructor(type) {
 		this.children = []
-		this.root = document.createElement('div')
+		this.root = document.createElement(type)
 	}
 	setAttribute(k, v) {
 		this.root.setAttribute(k, v)
@@ -22,22 +42,50 @@ class Component {
 	appendChild(child) {
 		this.children.push(child)
 	}
+	addEventListener() {
+		this.root.addEventListener(...arguments)
+	}
 	mountTo(parent) {
 		parent.appendChild(this.root)
 		this.children.forEach(child => child.mountTo(this.root))
 	}
 }
 
-const c = (
-	<Component
-		id='a'
-		class='b'
-		style='width:100px;height:100px;background-color:lightblue;'
-	>
-		<Component></Component>
-		<Component></Component>
-		<Component></Component>
-	</Component>
+class Carousel {
+	constructor() {
+		this.children = []
+		this.root = document.createElement('div')
+	}
+	setAttribute(k, v) {
+		this[k] = v
+	}
+	appendChild(child) {
+		this.children.push(child)
+	}
+	render() {
+		return (
+			<div id='container'>
+				{this.data.map(url => (
+					<img src={url} draggable={false} />
+				))}
+			</div>
+		)
+	}
+	mountTo(parent) {
+		this.render().mountTo(parent)
+	}
+}
+
+const carousel = (
+	<Carousel
+		data={[
+			'https://tse1-mm.cn.bing.net/th/id/OIP.z2Q5yDKbQakzgCcUrKBJkAHaJz?pid=Api&rs=1',
+			'https://tse4-mm.cn.bing.net/th/id/OIP.ZFVnyvQrDwUFrKd4a55VFAHaJ4?pid=Api&rs=1',
+			'https://tse4-mm.cn.bing.net/th/id/OIP.2Fxq5vDKSaEYuTXzKuw_gwHaJ4?pid=Api&rs=1',
+			'https://tse1-mm.cn.bing.net/th/id/OIP.diD6kXAYPVQN1I3YviPYJgHaFj?pid=Api&rs=1',
+			'https://ranking.xgoo.jp/tool/images/column/2020/01/0128_9.jpg'
+		]}
+	/>
 )
 
-c.mountTo(document.body)
+carousel.mountTo(document.body)
