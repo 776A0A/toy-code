53f4b62ff6ecf3a6b2afe4666ffa50a4ee2ad8b0
har3dy <har3dy@icloud.com>
2020-09-04 12:19:25 +0800

    添加carousel，完成基本搭建

--- component/main.js
+++ component/main.js
@@ -1,20 +1,43 @@
-function create(cls, attributes) {
+function create(cls, attributes, ...children) {
 	const c = new cls()
 	for (const key in attributes) {
 		if (attributes.hasOwnProperty(key)) {
-			const attr = attributes[key]
-			c[key] = attr
+			c.setAttribute(key, attributes[key])
 		}
+	}
+	for (const child of children) {
+		c.appendChild(child)
 	}
 	return c
 }
 
-class Div {}
+class Component {
+	constructor(config) {
+		this.children = []
+		this.root = document.createElement('div')
+	}
+	setAttribute(k, v) {
+		this.root.setAttribute(k, v)
+	}
+	appendChild(child) {
+		this.children.push(child)
+	}
+	mountTo(parent) {
+		parent.appendChild(this.root)
+		this.children.forEach(child => child.mountTo(this.root))
+	}
+}
 
 const c = (
-	<Div id='a' class='b'>
-		123
-	</Div>
+	<Component
+		id='a'
+		class='b'
+		style='width:100px;height:100px;background-color:lightblue;'
+	>
+		<Component></Component>
+		<Component></Component>
+		<Component></Component>
+	</Component>
 )
 
-console.log(c);
+c.mountTo(document.body)
