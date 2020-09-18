e54d8d7c4d97d64cdc949f5422195a10685622ce
har3dy <har3dy@icloud.com>
2020-09-18 13:01:44 +0800

    重写upload

--- component/uploader/src/preview.ts
+++
@@ -1,7 +0,0 @@
-export default file =>
-	new Promise((resolve, reject) => {
-		const reader = new FileReader()
-		reader.readAsDataURL(file)
-		reader.onload = e => resolve(e.target.result)
-		reader.onerror = reader.onabort = reject
-	})
---
+++ component/uploader/src/utils/index.js
@@ -0,0 +1,2 @@
+export const _ = selector => document.querySelector(selector)
+export const __ = (selector = document.querySelectorAll(selector))
