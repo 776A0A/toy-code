7d0b38355545a73d701beb771bb7446a3a82eb2c
Darenome <har3dy@icloud.com>
2021-04-25 14:49:36 +0800

    feat(canvas): stage可计算图形中心点位置

--- canvas/DrawingBoard/stage.js
+++ canvas/DrawingBoard/stage.js
@@ -6,7 +6,6 @@ import { ShapeBox } from './shapeBox.js'
 import { Switcher, modes } from './switcher.js'
 
 // TODO 旋转
-// TODO 添加文字
 // TODO 右键点击撤销
 export class Stage {
     constructor(canvas) {
@@ -93,4 +92,45 @@ export class Stage {
         }
         this.adder.switchTo[shape]()
     }
+    getEditingGraph() {
+        return this.shapeBox.shapes[this.editor.topGraphIndex]
+    }
+    getGraphCenter(graph) {
+        if (graph.name === 'rect') {
+            const { x, y, width, height } = graph
+            return [x + width / 2, y + height / 2]
+        } else if (graph.name === 'polygon') {
+            const center = calculateCenter(
+                graph.points.map(({ x, y }) => [x, y])
+            )
+            return center
+        }
+    }
+}
+
+function calculateCenter(points) {
+    let sum_x = 0
+    let sum_y = 0
+    let sum_area = 0
+    let point = points[1]
+    for (let i = 2; i < points.length; i++) {
+        const _point = points[i]
+        const area = calcArea(points[0], point, _point)
+        sum_area += area
+        sum_x += (points[0][0] + point[0] + _point[0]) * area
+        sum_y += (points[0][1] + point[1] + _point[1]) * area
+        point = _point
+    }
+    return [sum_x / sum_area / 3, sum_y / sum_area / 3]
+}
+function calcArea(p0, p1, p2) {
+    return (
+        (p0[0] * p1[1] +
+            p1[0] * p2[1] +
+            p2[0] * p0[1] -
+            p1[0] * p0[1] -
+            p2[0] * p1[1] -
+            p0[0] * p2[1]) /
+        2
+    )
 }
