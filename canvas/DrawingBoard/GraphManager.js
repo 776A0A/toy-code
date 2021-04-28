6547a904866743d07a30e5ee568c7ed01bbca07f
Darenome <har3dy@icloud.com>
2021-04-28 10:20:37 +0800

    feat(canvas): addGraph可添加第二个参数，控制插入位置

--- canvas/DrawingBoard/GraphManager.js
+++ canvas/DrawingBoard/GraphManager.js
@@ -2,14 +2,21 @@ export class GraphManager {
     constructor() {
         this._graphs = new Set()
     }
-    add(graph) {
-        this._graphs.add(graph)
+    add(graph, insertIndex) {
+        if (insertIndex !== undefined) {
+            const graphs = this.graphs
+            graphs.splice(insertIndex, 0, graph)
+            this.graphs = graphs
+        } else this._graphs.add(graph)
     }
     delete(graph) {
         this._graphs.delete(graph)
     }
     clear() {
         this._graphs.clear()
+    }
+    set graphs(graphs) {
+        this._graphs = new Set(graphs)
     }
     get graphs() {
         return [...this._graphs]
