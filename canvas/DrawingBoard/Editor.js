cffa6e40a93b45eee249e3b440dc09f0dc9384cd
Darenome <har3dy@icloud.com>
2021-05-10 11:59:35 +0800

    fix(canvas): 修复在导入导出时如果是编辑状态，需要退出编辑

--- canvas/DrawingBoard/Editor.js
+++ canvas/DrawingBoard/Editor.js
@@ -1,170 +1,175 @@
 import { events, cursors } from './shared.js'
-import { PolygonTransformer, RectTransformer, PictureTransformer } from './Transformers/index.js'
+import {
+  PolygonTransformer,
+  RectTransformer,
+  PictureTransformer,
+} from './Transformers/index.js'
 import { PluginHost } from './PluginHost.js'
 import { stageModes } from './Stage.js'
 
 export const editorModes = {
-    wait: Symbol('wait'), // 等在选择图形
-    resize: Symbol('resize'),
-    drag: Symbol('drag'),
+  wait: Symbol('wait'), // 等在选择图形
+  resize: Symbol('resize'),
+  drag: Symbol('drag'),
 }
 
 export class Editor extends PluginHost {
-    stage = null
-    topGraphIndex = undefined
-    isEditing = false // 点选到了某一个图形即为true
-    isDragging = false
-    isResizing = false
-    mode = editorModes.wait
-    transformers = new Map()
-    transformer = null
-    plugins = new Set()
-    constructor() {
-        super()
-        this.init()
+  stage = null
+  topGraphIndex = undefined
+  isEditing = false // 点选到了某一个图形即为true
+  isDragging = false
+  isResizing = false
+  mode = editorModes.wait
+  transformers = new Map()
+  transformer = null
+  plugins = new Set()
+  constructor() {
+    super()
+    this.init()
+  }
+  init() {
+    this.on({
+      type: events.DELETE_GRAPH,
+      handler: (graph) => {
+        this.stage.emit(events.DELETE_GRAPH, graph).display()
+        this.deleteGraph()
+      },
+    })
+  }
+  injectTransformer(name, transformer) {
+    if (this.transformers.has(name)) return
+    this.transformers.set(name, transformer)
+  }
+  setMode(mode) {
+    this.mode = mode
+
+    if (mode === editorModes.resize) {
+      this.isResizing = true
+      this.transformer.start()
+    } else if (mode === editorModes.drag) {
+      this.isDragging = true
+    } else if (mode === editorModes.wait) {
+      this.isResizing = this.isDragging = false
+      this.stage.emit(events.CHANGE_CURSOR, cursors.grab)
     }
-    init() {
-        this.on({
-            type: events.DELETE_GRAPH,
-            handler: (graph) => {
-                this.stage.emit(events.DELETE_GRAPH, graph).display()
-                this.deleteGraph()
-            },
-        })
+  }
+  edit({ x, y }) {
+    if (!this.isEditing || this.mode === editorModes.wait) return
+
+    if (this.mode === editorModes.resize && this.isResizing) {
+      this.transformer.resize({ x, y })
+    } else if (this.mode === editorModes.drag && this.isDragging) {
+      this.transformer.drag({ x, y })
+    } else throw Error(`没有这个编辑模式：${this.mode}`)
+
+    this.stage.emit(events.REFRESH_SCREEN)
+  }
+  pick({ x, y }) {
+    this.removeMenuIfHas()
+
+    const graphs = this.stage.graphManager.graphs
+    if (!graphs.length) return
+
+    if (this.transformer && this.transformer.isPicked({ x, y }) !== -1) {
+      this.setMode(editorModes.resize)
+      this.stage.emit(events.CHANGE_CURSOR, cursors.grabbing)
+    } else {
+      const top = this.findTop({ x, y })
+
+      // 没有选中过，什么都不做
+      if (top === undefined && this.topGraphIndex === undefined) return
+
+      this.transformer?.end()
+
+      if (top !== undefined) {
+        const graph = graphs[top]
+        this.transformer = this.transformers
+          .get(graph.name)
+          .generate(graph, { x, y })
+
+        this.isEditing = true
+        this.topGraphIndex = top
+        this.setMode(editorModes.drag)
+        this.stage.emit(events.CHANGE_CURSOR, cursors.grabbing)
+      } else {
+        this.end()
+        this.stage.emit(events.CHANGE_CURSOR, cursors.grab)
+      }
+
+      this.stage.emit(events.REFRESH_SCREEN)
     }
-    injectTransformer(name, transformer) {
-        if (this.transformers.has(name)) return
-        this.transformers.set(name, transformer)
+  }
+  // 删除图形
+  deleteGraph() {
+    this.topGraphIndex = undefined
+    this.transformer = null
+    this.setMode(editorModes.wait)
+  }
+  // 结束编辑，已经进入非编辑状态
+  end() {
+    this.transformer?.end()
+    this.removeMenuIfHas()
+    this.isEditing = this.isDragging = this.isResizing = false
+    this.topGraphIndex = undefined
+    this.transformer = null
+    this.stage.emit(events.REFRESH_SCREEN)
+  }
+  findTop({ x, y }) {
+    const graphs = this.stage.graphManager.graphs
+    if (!graphs.length) return
+
+    let top
+    ;[...graphs].forEach((graph, idx) => {
+      if (graph.isInPath(x, y)) top = idx
+    })
+
+    return top
+  }
+  removeMenuIfHas() {
+    if (this.transformer?.menu) {
+      this.transformer.menu.remove()
+      this.transformer.menu = null
     }
-    setMode(mode) {
-        this.mode = mode
+  }
+  handleContextmenu({ x, y, nativeEvent }) {
+    nativeEvent.preventDefault()
 
-        if (mode === editorModes.resize) {
-            this.isResizing = true
-            this.transformer.start()
-        } else if (mode === editorModes.drag) {
-            this.isDragging = true
-        } else if (mode === editorModes.wait) {
-            this.isResizing = this.isDragging = false
-            this.stage.emit(events.CHANGE_CURSOR, cursors.grab)
-        }
+    if (this.topGraphIndex === undefined) return
+
+    this.transformer.handleContextmenu({ x, y, nativeEvent })
+  }
+  install(stage) {
+    this.stage = stage
+
+    stage
+      .on({
+        type: 'mousedown',
+        handler: (event) => check() && this.pick(event),
+      })
+      .on({
+        type: 'mousemove',
+        handler: (event) => check() && this.edit(event),
+      })
+      .on({
+        type: 'mouseup',
+        handler: () => check() && this.setMode(editorModes.wait),
+      })
+      .on({
+        type: 'mouseleave',
+        handler: () => check() && this.setMode(editorModes.wait),
+      })
+      .on({
+        type: 'contextmenu',
+        handler: (event) => check() && this.handleContextmenu(event),
+      })
+      .on({ type: events.END_EDIT, handler: () => this.end() })
+      .on({ type: events.EXPORT, handler: () => this.end() })
+      .on({ type: events.IMPORT, handler: () => this.end() })
+
+    function check() {
+      return stage.mode === stageModes.editor
     }
-    edit({ x, y }) {
-        if (!this.isEditing || this.mode === editorModes.wait) return
-
-        if (this.mode === editorModes.resize && this.isResizing) {
-            this.transformer.resize({ x, y })
-        } else if (this.mode === editorModes.drag && this.isDragging) {
-            this.transformer.drag({ x, y })
-        } else throw Error(`没有这个编辑模式：${this.mode}`)
-
-        this.stage.emit(events.REFRESH_SCREEN)
-    }
-    pick({ x, y }) {
-        this.removeMenuIfHas()
-
-        const graphs = this.stage.graphManager.graphs
-        if (!graphs.length) return
-
-        if (this.transformer && this.transformer.isPicked({ x, y }) !== -1) {
-            this.setMode(editorModes.resize)
-            this.stage.emit(events.CHANGE_CURSOR, cursors.grabbing)
-        } else {
-            const top = this.findTop({ x, y })
-
-            // 没有选中过，什么都不做
-            if (top === undefined && this.topGraphIndex === undefined) return
-
-            this.transformer?.end()
-
-            if (top !== undefined) {
-                const graph = graphs[top]
-                this.transformer = this.transformers.get(graph.name).generate(graph, { x, y })
-
-                this.isEditing = true
-                this.topGraphIndex = top
-                this.setMode(editorModes.drag)
-                this.stage.emit(events.CHANGE_CURSOR, cursors.grabbing)
-            } else {
-                this.isEditing = false
-                this.topGraphIndex = undefined
-                this.stage.emit(events.CHANGE_CURSOR, cursors.grab)
-            }
-
-            this.stage.emit(events.REFRESH_SCREEN)
-        }
-    }
-    // 删除图形
-    deleteGraph() {
-        this.topGraphIndex = undefined
-        this.transformer = null
-        this.setMode(editorModes.wait)
-    }
-    // 结束编辑，已经进入非编辑状态
-    end() {
-        if (this.transformer) {
-            this.transformer.end()
-            this.stage.emit(events.REFRESH_SCREEN)
-        }
-        this.removeMenuIfHas()
-        this.isEditing = this.isDragging = this.isResizing = false
-        this.topGraphIndex = undefined
-        this.transformer = null
-    }
-    findTop({ x, y }) {
-        const graphs = this.stage.graphManager.graphs
-        if (!graphs.length) return
-
-        let top
-        ;[...graphs].forEach((graph, idx) => {
-            if (graph.isInPath(x, y)) top = idx
-        })
-
-        return top
-    }
-    removeMenuIfHas() {
-        if (this.transformer?.menu) {
-            this.transformer.menu.remove()
-            this.transformer.menu = null
-        }
-    }
-    handleContextmenu({ x, y, nativeEvent }) {
-        nativeEvent.preventDefault()
-
-        if (this.topGraphIndex === undefined) return
-
-        this.transformer.handleContextmenu({ x, y, nativeEvent })
-    }
-    install(stage) {
-        this.stage = stage
-
-        stage
-            .on({
-                type: 'mousedown',
-                handler: (event) => check() && this.pick(event),
-            })
-            .on({
-                type: 'mousemove',
-                handler: (event) => check() && this.edit(event),
-            })
-            .on({
-                type: 'mouseup',
-                handler: () => check() && this.setMode(editorModes.wait),
-            })
-            .on({
-                type: 'mouseleave',
-                handler: () => check() && this.setMode(editorModes.wait),
-            })
-            .on({
-                type: 'contextmenu',
-                handler: (event) => check() && this.handleContextmenu(event),
-            })
-            .on({ type: events.END_EDIT, handler: () => this.end() })
-
-        function check() {
-            return stage.mode === stageModes.editor
-        }
-    }
+  }
 }
 
 export const editor = new Editor()
--- canvas/DrawingBoard/Stage.js
+++ canvas/DrawingBoard/Stage.js
@@ -13,295 +13,305 @@ import { VpController } from './VpController.js'
 const LEFT_MOUSE_DOWN = 0
 
 export const stageModes = {
-    adder: Symbol('adder'),
-    editor: Symbol('editor'),
+  adder: Symbol('adder'),
+  editor: Symbol('editor'),
 }
 
 export class Stage extends EventEmitter {
-    mode = stageModes.adder
-    plugins = new Set()
-    vpControl = true
-    vpController = new VpController()
-    cursor = cursors.crosshair
-    constructor(canvas) {
-        super()
+  mode = stageModes.adder
+  plugins = new Set()
+  vpControl = true
+  vpController = new VpController()
+  cursor = cursors.crosshair
+  constructor(canvas) {
+    super()
 
-        this.canvas = canvas
-        this._display = new Display(canvas)
-        this.graphManager = new GraphManager(this)
+    this.canvas = canvas
+    this._display = new Display(canvas)
+    this.graphManager = new GraphManager(this)
 
-        if (!this.vpControl) {
-            this.vpController = null
-        } else {
-            this.vpController.install(this)
+    if (!this.vpControl) {
+      this.vpController = null
+    } else {
+      this.vpController.install(this)
+    }
+
+    this.init()
+  }
+  init() {
+    this.addListener()
+    this.setCursor(cursors.crosshair)
+  }
+  // TODO 处理retina问题
+  initRetina() {
+    const dpi = window.devicePixelRatio
+    const canvas = this.canvas
+
+    canvas.style.width = `${canvas.width}px`
+    canvas.style.height = `${canvas.height}px`
+
+    canvas.width = canvas.width * dpi
+    canvas.height = canvas.height * dpi
+
+    canvas.getContext('2d').scale(dpi, dpi)
+  }
+  setMode(mode) {
+    this.mode = mode
+
+    if (mode === stageModes.adder) {
+      this.emit(events.END_EDIT)
+      this.setCursor(cursors.crosshair)
+    } else this.setCursor(cursors.grab)
+
+    return this
+  }
+  use(plugin) {
+    if (this.plugins.has(plugin)) return
+    this.plugins.add(plugin)
+    plugin.install(this)
+    return this
+  }
+  get handlers() {
+    return {
+      handleMouseDown: (evt) => {
+        if (evt.button !== LEFT_MOUSE_DOWN) return
+
+        const params = generateParams.call(
+          this,
+          evt,
+          this.vpController.getTransformedPosition(evt.offsetX, evt.offsetY)
+        )
+
+        if (this.vpController.isSpaceDown)
+          this.vpController.handleMouseDown(params)
+        else this.emit('mousedown', params)
+      },
+      handleMouseMove: (evt) => {
+        const params = generateParams.call(
+          this,
+          evt,
+          this.vpController.getTransformedPosition(evt.offsetX, evt.offsetY)
+        )
+
+        this.emit('mousemove', params)
+      },
+      handleMouseUp: (evt) => {
+        const params = generateParams.call(this, evt)
+        this.emit('mouseup', params)
+      },
+      handleMouseLeave: (evt) => {
+        const params = generateParams.call(this, evt)
+        this.emit('mouseleave', params)
+      },
+      handleDblClick: (evt) => {
+        const params = generateParams.call(this, evt)
+        this.emit('dblclick', params)
+      },
+      handleContextMenu: (evt) => {
+        const params = generateParams.call(
+          this,
+          evt,
+          this.vpController.getTransformedPosition(evt.clientX, evt.clientY)
+        )
+
+        this.emit('contextmenu', params)
+      },
+      handleWheel: (evt) => {
+        const params = generateParams.call(this, evt)
+        this.emit('wheel', params)
+      },
+      handleKeyDown: (evt) => {
+        const params = generateParams.call(this, evt)
+        this.emit('keydown', params)
+      },
+      handleKeyUp: (evt) => {
+        const params = generateParams.call(this, evt)
+        this.emit('keyup', params)
+      },
+    }
+  }
+  addListener() {
+    this.on({
+      elem: this.canvas,
+      type: 'mousedown',
+      handler: this.handlers.handleMouseDown,
+    })
+      .on({
+        elem: this.canvas,
+        type: 'mousemove',
+        handler: this.handlers.handleMouseMove,
+      })
+      .on({
+        elem: this.canvas,
+        type: 'mouseup',
+        handler: this.handlers.handleMouseUp,
+      })
+      .on({
+        elem: this.canvas,
+        type: 'mouseleave',
+        handler: this.handlers.handleMouseLeave,
+      })
+      .on({
+        elem: this.canvas,
+        type: 'dblclick',
+        handler: this.handlers.handleDblClick,
+      })
+      .on({
+        elem: this.canvas,
+        type: 'contextmenu',
+        handler: this.handlers.handleContextMenu,
+      })
+      .on({
+        elem: this.canvas,
+        type: 'wheel',
+        handler: this.handlers.handleWheel,
+      })
+      .on({
+        elem: document,
+        type: 'keydown',
+        handler: this.handlers.handleKeyDown,
+      })
+      .on({
+        elem: document,
+        type: 'keyup',
+        handler: this.handlers.handleKeyUp,
+      })
+      .on({
+        type: events.ADD_GRAPH,
+        handler: (graph, insertIndex) => this.addGraph(graph, insertIndex),
+      })
+      .on({
+        type: events.DELETE_GRAPH,
+        handler: (graph) => this.deleteGraph(graph),
+      })
+      .on({ type: events.REFRESH_SCREEN, handler: () => this.display() })
+      .on({
+        type: events.CHANGE_CURSOR,
+        handler: (cursor) => this.setCursor(cursor),
+      })
+  }
+  addGraph(graph, insertIndex) {
+    this.graphManager.add(graph, insertIndex)
+    return this
+  }
+  deleteGraph(graph) {
+    this.graphManager.delete(graph)
+
+    return this
+  }
+  display() {
+    if (this.vpControl) {
+      this.vpController.scale(() => {
+        this._display.refresh(this.graphManager.graphs)
+      })
+    } else {
+      this._display.refresh(this.graphManager.graphs)
+    }
+    return this
+  }
+  refresh() {
+    this.display()
+    return this
+  }
+  import(graphs) {
+    this.emit(events.IMPORT)
+    
+    const constructorMap = {
+      rect: Rect,
+      polygon: Polygon,
+      text: Text,
+      circle: Circle,
+      point: Point,
+      picture: Picture,
+    }
+    const ctx = this.canvas.getContext('2d')
+    let a = 0,
+      b = 0
+
+    const generate = (graphs, parent) => {
+      graphs.forEach(({ attrs, name, children }) => {
+        const constructor = constructorMap[name]
+
+        if (name === 'picture') {
+          a++
+          const image = new Image()
+          image.src = attrs.image
+          image.crossOrigin = 'anonymous'
+          attrs.image = image
+          image.onload = () => b++
         }
 
-        this.init()
-    }
-    init() {
-        this.addListener()
-        this.setCursor(cursors.crosshair)
-    }
-    // TODO 处理retina问题
-    initRetina() {
-        const dpi = window.devicePixelRatio
-        const canvas = this.canvas
+        const graph = new constructor({ ctx, ...attrs })
 
-        canvas.style.width = `${canvas.width}px`
-        canvas.style.height = `${canvas.height}px`
+        if (children.length) generate(children, graph)
 
-        canvas.width = canvas.width * dpi
-        canvas.height = canvas.height * dpi
-
-        canvas.getContext('2d').scale(dpi, dpi)
-    }
-    setMode(mode) {
-        this.mode = mode
-
-        if (mode === stageModes.adder) {
-            this.emit(events.END_EDIT)
-            this.setCursor(cursors.crosshair)
-        } else this.setCursor(cursors.grab)
-
-        return this
-    }
-    use(plugin) {
-        if (this.plugins.has(plugin)) return
-        this.plugins.add(plugin)
-        plugin.install(this)
-        return this
-    }
-    get handlers() {
-        return {
-            handleMouseDown: (evt) => {
-                if (evt.button !== LEFT_MOUSE_DOWN) return
-
-                const params = generateParams.call(
-                    this,
-                    evt,
-                    this.vpController.getTransformedPosition(evt.offsetX, evt.offsetY)
-                )
-
-                if (this.vpController.isSpaceDown) this.vpController.handleMouseDown(params)
-                else this.emit('mousedown', params)
-            },
-            handleMouseMove: (evt) => {
-                const params = generateParams.call(
-                    this,
-                    evt,
-                    this.vpController.getTransformedPosition(evt.offsetX, evt.offsetY)
-                )
-
-                this.emit('mousemove', params)
-            },
-            handleMouseUp: (evt) => {
-                const params = generateParams.call(this, evt)
-                this.emit('mouseup', params)
-            },
-            handleMouseLeave: (evt) => {
-                const params = generateParams.call(this, evt)
-                this.emit('mouseleave', params)
-            },
-            handleDblClick: (evt) => {
-                const params = generateParams.call(this, evt)
-                this.emit('dblclick', params)
-            },
-            handleContextMenu: (evt) => {
-                const params = generateParams.call(
-                    this,
-                    evt,
-                    this.vpController.getTransformedPosition(evt.clientX, evt.clientY)
-                )
-
-                this.emit('contextmenu', params)
-            },
-            handleWheel: (evt) => {
-                const params = generateParams.call(this, evt)
-                this.emit('wheel', params)
-            },
-            handleKeyDown: (evt) => {
-                const params = generateParams.call(this, evt)
-                this.emit('keydown', params)
-            },
-            handleKeyUp: (evt) => {
-                const params = generateParams.call(this, evt)
-                this.emit('keyup', params)
-            },
-        }
-    }
-    addListener() {
-        this.on({
-            elem: this.canvas,
-            type: 'mousedown',
-            handler: this.handlers.handleMouseDown,
-        })
-            .on({
-                elem: this.canvas,
-                type: 'mousemove',
-                handler: this.handlers.handleMouseMove,
-            })
-            .on({
-                elem: this.canvas,
-                type: 'mouseup',
-                handler: this.handlers.handleMouseUp,
-            })
-            .on({
-                elem: this.canvas,
-                type: 'mouseleave',
-                handler: this.handlers.handleMouseLeave,
-            })
-            .on({
-                elem: this.canvas,
-                type: 'dblclick',
-                handler: this.handlers.handleDblClick,
-            })
-            .on({
-                elem: this.canvas,
-                type: 'contextmenu',
-                handler: this.handlers.handleContextMenu,
-            })
-            .on({
-                elem: this.canvas,
-                type: 'wheel',
-                handler: this.handlers.handleWheel,
-            })
-            .on({
-                elem: document,
-                type: 'keydown',
-                handler: this.handlers.handleKeyDown,
-            })
-            .on({
-                elem: document,
-                type: 'keyup',
-                handler: this.handlers.handleKeyUp,
-            })
-            .on({
-                type: events.ADD_GRAPH,
-                handler: (graph, insertIndex) => this.addGraph(graph, insertIndex),
-            })
-            .on({
-                type: events.DELETE_GRAPH,
-                handler: (graph) => this.deleteGraph(graph),
-            })
-            .on({ type: events.REFRESH_SCREEN, handler: () => this.display() })
-            .on({ type: events.CHANGE_CURSOR, handler: (cursor) => this.setCursor(cursor) })
-    }
-    addGraph(graph, insertIndex) {
-        this.graphManager.add(graph, insertIndex)
-        return this
-    }
-    deleteGraph(graph) {
-        this.graphManager.delete(graph)
-
-        return this
-    }
-    display() {
-        if (this.vpControl) {
-            this.vpController.scale(() => {
-                this._display.refresh(this.graphManager.graphs)
-            })
-        } else {
-            this._display.refresh(this.graphManager.graphs)
-        }
-        return this
-    }
-    refresh() {
-        this.display()
-    }
-    import(graphs) {
-        const constructorMap = {
-            rect: Rect,
-            polygon: Polygon,
-            text: Text,
-            circle: Circle,
-            point: Point,
-            picture: Picture,
-        }
-        const ctx = this.canvas.getContext('2d')
-        let a = 0,
-            b = 0
-
-        const generate = (graphs, parent) => {
-            graphs.forEach(({ attrs, name, children }) => {
-                const constructor = constructorMap[name]
-
-                if (name === 'picture') {
-                    a++
-                    const image = new Image()
-                    image.src = attrs.image
-                    image.crossOrigin = 'anonymous'
-                    attrs.image = image
-                    image.onload = () => b++
-                }
-
-                const graph = new constructor({ ctx, ...attrs })
-
-                if (children.length) generate(children, graph)
-
-                if (name === 'polygon') {
-                    const points = attrs.points.map((point) => new Point({ ctx, ...point.attrs }))
-                    graph.attr({ points }).updatePointsDiff()
-                }
-
-                if (parent) parent.appendChild(graph)
-                else this.addGraph(graph)
-            })
+        if (name === 'polygon') {
+          const points = attrs.points.map(
+            (point) => new Point({ ctx, ...point.attrs })
+          )
+          graph.attr({ points }).updatePointsDiff()
         }
 
-        generate(graphs)
-
-        const d = () => {
-            if (a === b) this.display()
-            else setTimeout(d, 50)
-        }
-
-        setTimeout(d, 50)
+        if (parent) parent.appendChild(graph)
+        else this.addGraph(graph)
+      })
     }
-    // TODO 导出数据中增加canvas的宽高
-    export() {
-        const graphs = this.graphManager.graphs
-        const shakenGraphs = shake(graphs)
-        return shakenGraphs
 
-        function shake(graphs) {
-            return graphs.map((graph) => {
-                const { ctx, ...attrs } = graph.attrs
+    generate(graphs)
 
-                const { name, children } = graph
-
-                if (name === 'picture') attrs.image = attrs.image.src
-                if (name === 'polygon')
-                    attrs.points = attrs.points.map((point) => {
-                        const { ctx, ...attrs } = point.attrs
-                        const { name, children } = point
-
-                        return { name, children, attrs }
-                    })
-
-                const obj = { attrs, name, children }
-
-                if (children.length) obj.children = shake(obj.children)
-
-                return obj
-            })
-        }
+    const d = () => {
+      if (a === b) this.display()
+      else setTimeout(d, 50)
     }
-    setCursor(cursor) {
-        if (!cursors) throw Error('请传入合法的 cursor 值！')
 
-        if (cursor === cursors.crosshair) {
-            if (this.mode === stageModes.editor) cursor = cursors.grab
-        }
+    setTimeout(d, 50)
+  }
+  // TODO 导出数据中增加canvas的宽高
+  export() {
+    this.emit(events.EXPORT)
+    const graphs = this.graphManager.graphs
+    const shakenGraphs = shake(graphs)
+    return shakenGraphs
 
-        this.canvas.style.cursor = cursor
+    function shake(graphs) {
+      return graphs.map((graph) => {
+        const { ctx, ...attrs } = graph.attrs
+
+        const { name, children } = graph
+
+        if (name === 'picture') attrs.image = attrs.image.src
+        if (name === 'polygon')
+          attrs.points = attrs.points.map((point) => {
+            const { ctx, ...attrs } = point.attrs
+            const { name, children } = point
+
+            return { name, children, attrs }
+          })
+
+        const obj = { attrs, name, children }
+
+        if (children.length) obj.children = shake(obj.children)
+
+        return obj
+      })
     }
+  }
+  setCursor(cursor) {
+    if (!cursors) throw Error('请传入合法的 cursor 值！')
+
+    if (cursor === cursors.crosshair) {
+      if (this.mode === stageModes.editor) cursor = cursors.grab
+    }
+
+    this.canvas.style.cursor = cursor
+  }
 }
 
 function generateParams(evt, params) {
-    return {
-        type: evt.type,
-        nativeEvent: evt,
-        graphs: this.graphManager.graphs,
-        ...params,
-    }
+  return {
+    type: evt.type,
+    nativeEvent: evt,
+    graphs: this.graphManager.graphs,
+    ...params,
+  }
 }
